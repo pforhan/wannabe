02e50cb7ad00dde82541546ad5d09b58255ceeac
Patrick Forhan <pforhan@squareup.com>
2020-06-17 16:59:32 -0500

    More EOL

--- library/src/main/java/wannabe/grid/iterators/EmptyIterator.kt
+++
@@ -1,9 +0,0 @@
-package wannabe.grid.iterators
-
-import java.util.NoSuchElementException
-
-class EmptyIterator<T> : Iterator<T> {
-  override fun hasNext(): Boolean = false
-
-  override fun next(): T = throw NoSuchElementException("Empty iterator has no elements")
-}
